ed973e1a2f63e71712ecfb8d45f14e1bc16c09ce
Bruno Antunes <bmvantunes@users.noreply.github.com>
2021-11-02 20:41:40 +0000

    change

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,11 +1,18 @@
 // pages/_middleware.ts
 
-import type { NextFetchEvent, NextRequest } from 'next/server'
-import { NextResponse } from 'next/server';
+import type { NextFetchEvent, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log('BRUNOOOOOOOOOOOOOOOxx222x', req, 'xxxxxxxxxxxxx', ev);
-  
-  
+  console.log("BRUNOOOOOOOOOOOOOOOxx222xgeo", 
+  JSON.stringify(req.geo), 
+  "xxxxxxxxxxxxxbody", 
+  req.body,
+  "xxxxxxxxxxxxxnexturl", 
+  JSON.stringify(req.nextUrl),"xxxxxxxxxxxxxbodyjson", 
+  JSON.stringify(req.body),"xxxxxxxxxxxxxip", 
+  JSON.stringify(req.ip),"xxxxxxxxxxxxxua", 
+  JSON.stringify(req.ua),);
+
   NextResponse.next();
 }
