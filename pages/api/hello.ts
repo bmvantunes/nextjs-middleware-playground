24b15a5dca1b6e24a88cb5c3581e050e966925aa
Bruno Antunes <bmvantunes@users.noreply.github.com>
2021-11-02 21:14:57 +0000

    change

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,5 +10,5 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   console.log('YEPPPPPPP', JSON.stringify(req.body));
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: req.body})
 }
