ef778d3a4773beeddda00d430745850c5802e928
Bruno Antunes <bmvantunes@users.noreply.github.com>
2021-11-02 20:31:03 +0000

    change

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,8 +1,11 @@
 // pages/_middleware.ts
 
 import type { NextFetchEvent, NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log('BRUNOOOOOOOOOOOOOOOxxx', req, 'xxxxxxxxxxxxx', ev);
-  return new Response('Hello, world!2')
+  console.log('BRUNOOOOOOOOOOOOOOOxx222x', req, 'xxxxxxxxxxxxx', ev);
+  
+  
+  NextResponse.next();
 }
